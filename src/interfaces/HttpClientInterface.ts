6ed94c575e113956e459d79510d8be9c7054961c
Francisco Alfredo Jarma <franciscoalfredojarma@gmail.com>
2024-09-03 10:44:28 -0300

    feat(BELLY-03): add esLint and prettier

--- src/interfaces/HttpClientInterface.ts
+++ src/interfaces/HttpClientInterface.ts
@@ -1,8 +1,16 @@
-import { AxiosRequestConfig } from "axios";
+import {AxiosRequestConfig} from "axios";
 
 export interface HttpClientInterface {
-  get<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
-  post<T>(url: string, data?: unknown, config?: AxiosRequestConfig): Promise<T>;
-  put<T>(url: string, data?: unknown, config?: AxiosRequestConfig): Promise<T>;
-  delete<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
+    get<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
+    post<T>(
+        url: string,
+        data?: unknown,
+        config?: AxiosRequestConfig
+    ): Promise<T>;
+    put<T>(
+        url: string,
+        data?: unknown,
+        config?: AxiosRequestConfig
+    ): Promise<T>;
+    delete<T>(url: string, config?: AxiosRequestConfig): Promise<T>;
 }
